6550bd97094be4fb13f87774f2b543cdbcf48443
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-05-30 14:21:02 +0100

    move api endpoint to env var

--- frontend/src/api/api-config.ts
+++ frontend/src/api/api-config.ts
@@ -7,7 +7,7 @@ const API = () => {
     const navigate = useNavigate();
 
 
-    const backendHost = "http://localhost:3001";
+    const backendHost = import.meta.env.VITE_SOME_KEY;
     // const backendHost = "http://10.0.12.7:3001";
     // const backendHost = "http://192.168.0.102:3001";
     // const backendHost = "https://link-sharing-app-server-4tmo.onrender.com"
