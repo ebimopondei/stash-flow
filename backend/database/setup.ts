22bf795729f6f4d1435b8affe99fbf9f571b86cf
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-05-30 12:47:51 +0100

    allow secure connection

--- backend/database/setup.ts
+++ backend/database/setup.ts
@@ -3,15 +3,28 @@ import { Dialect} from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize( {
-database: process.env.DB_NAME,
+const baseConfig = {
+  database: process.env.DB_NAME,
   username: process.env.DB_USER,
   password: process.env.DB_PASS,
   host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT),
   dialect: process.env.DB_DIALECT as Dialect,
-  logging: true
-});
+  logging: false
+};
+
+const sequelizeConfig: any = { ...baseConfig };
+
+if (process.env.NODE_ENV === 'production') {
+  sequelizeConfig.dialectOptions = {
+    ssl: {
+      require: true, // Enforce SSL connection
+      rejectUnauthorized: true, 
+    }
+  };
+}
+
+const sequelize = new Sequelize(sequelizeConfig);
 
 sequelize.sync();
 
