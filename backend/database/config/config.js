45d0d13bdd265fa6f53c92ee010094602ad6747c
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-05-30 14:08:32 +0100

    OMOH

--- backend/database/config/config.js
+++ backend/database/config/config.js
@@ -1,17 +1,18 @@
 require('ts-node/register');
 
-module.exports = {
-  
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
-  },
-  
-  test: {
+const development= {
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  dialect: process.env.DB_DIALECT
+}
+
+
+
+
+const test = {
     username: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DB_NAME,
@@ -19,24 +20,36 @@ module.exports = {
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
     dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false, // Required for Neon or some managed PG services
-      }
-    },
+        ssl: {
+            require: true,
+        rejectUnauthorized: false, // Required for Neon or some managed PG services
+    }
   },
-  production: {
+}
+
+const production = {
     username: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT,
-    dialectOptions: {
-      ssl: {
-          require: true,
-          rejectUnauthorized: false, // Required for Neon or some managed PG services
-      }
-    },
-  },
+    dialect: process.env.DB_DIALECT
+}
+
+
+if (process.env.NODE_ENV === 'production') {
+production.dialectOptions = {
+  ssl: {
+    require: true, // Enforce SSL connection
+    rejectUnauthorized: true, 
+  }
+};
+}
+
+module.exports = {
+    
+    production,
+    test,
+    development
+  
 };
