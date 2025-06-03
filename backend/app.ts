a4d7af4b4c3f3aa285be4084cc9d7523e974ff78
Pondei Ebimobowei <ebi4jah15@gmail.com>
2025-06-03 17:19:23 +0100

    deploy with 3

--- backend/app.ts
+++ backend/app.ts
@@ -10,7 +10,7 @@ import { PORT } from './config/server'
 import userAgent  from 'express-useragent'
 
 const app = express()
-app.set('trust proxy', 2);
+app.set('trust proxy', 3);
 app.use(userAgent.express());
 
 app.use(express.urlencoded( { extended: true, }))
